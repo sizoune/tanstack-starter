92a9054cb6b2d77cde4eef572df7121570cd574c
Jacky Tea <44061647+JackyTea@users.noreply.github.com>
2025-11-09 18:29:40 -0500

    feat(app): add welcome username text on login

--- src/lib/pages/Home/Home.tsx
+++ src/lib/pages/Home/Home.tsx
@@ -1,13 +1,15 @@
 import { useTranslation } from 'react-i18next'
 import { AppLayout } from '@/layouts/AppLayout/AppLayout'
+import { Route as RootRoute } from '@/routes/__root'
 
 const Home = () => {
   const { t } = useTranslation()
+  const { user } = RootRoute.useRouteContext()
 
   return (
     <AppLayout>
       <div className="flex h-full w-full flex-col items-center justify-center">
-        <h1 className="mb-3 font-black text-2xl">🏝️ {t('Hello!')} 🏝️</h1>
+        <h1 className="mb-3 font-black text-2xl">🏝️ {user ? `${t('Welcome')}, ${user.name}!` : t('Hello!')} 🏝️</h1>
       </div>
     </AppLayout>
   )
