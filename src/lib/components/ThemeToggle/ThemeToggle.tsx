e0357a0dd8e3f37fd29717302e2154ed999a6e81
Jacky Tea <44061647+jackytea@users.noreply.github.com>
2026-01-03 21:05:34 -0500

    chore(lint): configure no unused imports rule

--- src/lib/components/ThemeToggle/ThemeToggle.tsx
+++ src/lib/components/ThemeToggle/ThemeToggle.tsx
@@ -5,7 +5,7 @@ import { toggleTheme } from '@/components/ThemeToggle/ThemeToggle.utils'
 const ThemeToggle = () => {
   return (
     <div className="flex cursor-pointer items-center justify-center" onClick={toggleTheme}>
-      <Sun className="dark:-rotate-90 size-6 rotate-0 scale-100 transition-all dark:scale-0" />
+      <Sun className="size-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute size-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle Theme</span>
     </div>
